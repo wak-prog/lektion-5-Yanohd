503786f687833416325a0838b036ff549cbb7f32
Yamen <yamen3mro@gmail.com>
2020-06-16 01:59:58 +0200

    l

--- Expression.c
+++ Expression.c
@@ -8,20 +8,24 @@ int a;
 int b;
 int c;
 int d;
-int E;
+int e;
+int f;
+e = a * b; 
+f = c * d;
+
 
 printf("Erste Zahl: ");
 scanf("%d", &a);
 printf("Zweite Zahl: ");
 scanf("%d", &b);
-printf ("Ergebnis Erste Zahle * Zweite Zahle: %d \n", a*b);
+printf ("Ergebnis Erste Zahle * Zweite Zahle: %d \n", e);
 printf("Dritte Zahl: ");
 scanf("%d", &c);
 printf("Vierte Zahl: ");
 scanf("%d", &d);
-printf ("Ergebnis Dritte Zahle * Viertte Zahle: %d \n", c*d);
-E = Expression (a, b, c, d);
+printf ("Ergebnis Dritte Zahle * Viertte Zahle: %d \n", f);
 
-printf ("Ergebnis: %d \n", a*b + c*d);
+
+printf ("Ergebnis: %d \n", e * f);
 return 0;
 }
--- signum.c
+++ signum.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+int signum(int a){
+
+    if (a >=1) {
+        return 1;
+    }
+    else if(a<=-1){
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
 
 int main() {
 int a;
-
-printf("Bitte einen Zahl eingeben: ");
+ 
+printf("Bitte einen Zahl eingeben: \n");
 scanf("%d", &a);
+printf ("%d \n", signum(a));
 
-if (a >=1) {
-printf ("Ergebnis: %d \n", 1);
-}
-else if(a<=-1){
-    printf ("Ergebnis: %d \n",-1);
-}
-else {
-    printf ("Ergebnis: %d \n", 0);
-}
+
 return 0;
 }
